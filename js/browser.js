aa7783b031146bfd4a8169a5a8c61be05d1dafb0
13023335265 <suchenglong018@126.com>
2023-06-18 16:19:21 +0800

    🦄 refactor: 重构和更新
    重构了机架功能的初始化事件绑定 补充了音频路由图

--- js/browser.js
+++ js/browser.js
@@ -2,71 +2,6 @@ var ClapBufferNode = new AudioBufferSourceNode(ctx)
 var KickBufferNode = new AudioBufferSourceNode(ctx)
 var SnareBufferNode = new AudioBufferSourceNode(ctx)
 var HitHatBufferNode = new AudioBufferSourceNode(ctx)
-// 按这个顺序
-var kick_buffer
-var clap_buffer
-var hiHat_buffer
-var snare_buffer
-
-var kick_rq = new XMLHttpRequest()
-kick_rq.responseType="arraybuffer"
-kick_rq.open("GET","media/Basic 808 Kick.wav")
-kick_rq.onload = function(event)
-{
-    // console.log(event)
-    ctx.decodeAudioData(kick_rq.response,(data)=>
-    {
-        kick_buffer = data
-        // ClapBufferNode.buffer=data
-        console.log(data);
-    })
-}
-kick_rq.send()
-
-var clap_rq = new XMLHttpRequest()
-clap_rq.responseType="arraybuffer"
-clap_rq.open("GET","media/Basic 808 Clap.wav")
-clap_rq.onload = function(event)
-{
-    // console.log(event)
-    ctx.decodeAudioData(clap_rq.response,(data)=>
-    {
-        clap_buffer = data
-        // ClapBufferNode.buffer=data
-        console.log(data);
-    })
-}
-clap_rq.send()
-
-var hihat_rq = new XMLHttpRequest()
-hihat_rq.responseType="arraybuffer"
-hihat_rq.open("GET","media/Basic 808 HiHat.wav")
-hihat_rq.onload = function(event)
-{
-    // console.log(event)
-    ctx.decodeAudioData(hihat_rq.response,(data)=>
-    {
-        hiHat_buffer = data
-        // ClapBufferNode.buffer=data
-        console.log(data);
-    })
-}
-hihat_rq.send()
-
-var snare_rq = new XMLHttpRequest()
-snare_rq.responseType="arraybuffer"
-snare_rq.open("GET","media/Basic 808 Snare.wav")
-snare_rq.onload = function(event)
-{
-    // console.log(event)
-    ctx.decodeAudioData(snare_rq.response,(data)=>
-    {
-        snare_buffer = data
-        // ClapBufferNode.buffer=data
-        console.log(data);
-    })
-}
-snare_rq.send()
 
 const browser = document.getElementsByClassName("browser").item(0)
 const file_wrapper = browser.children.item(0)
@@ -95,7 +30,7 @@ for(var i=0;i<file_wrapper.children.length;i++)
         if(c == "Basic 808 HiHat")
         {
             HitHatBufferNode = ctx.createBufferSource()
-            HitHatBufferNode.buffer = hiHat_buffer
+            HitHatBufferNode.buffer = hihat_buffer
             HitHatBufferNode.connect(ctx.destination)
             HitHatBufferNode.start()
         }
---
+++ preload.js
@@ -0,0 +1,64 @@
+var kick_buffer
+var clap_buffer
+var hihat_buffer
+var snare_buffer
+
+var kick_rq = new XMLHttpRequest()
+kick_rq.responseType="arraybuffer"
+kick_rq.open("GET","media/Basic 808 Kick.wav")
+kick_rq.onload = function(event)
+{
+    // console.log(event)
+    ctx.decodeAudioData(kick_rq.response,(data)=>
+    {
+        kick_buffer = data
+        // ClapBufferNode.buffer=data
+        console.log(data);
+    })
+}
+kick_rq.send()
+
+var clap_rq = new XMLHttpRequest()
+clap_rq.responseType="arraybuffer"
+clap_rq.open("GET","media/Basic 808 Clap.wav")
+clap_rq.onload = function(event)
+{
+    // console.log(event)
+    ctx.decodeAudioData(clap_rq.response,(data)=>
+    {
+        clap_buffer = data
+        // ClapBufferNode.buffer=data
+        console.log(data);
+    })
+}
+clap_rq.send()
+
+var hihat_rq = new XMLHttpRequest()
+hihat_rq.responseType="arraybuffer"
+hihat_rq.open("GET","media/Basic 808 HiHat.wav")
+hihat_rq.onload = function(event)
+{
+    // console.log(event)
+    ctx.decodeAudioData(hihat_rq.response,(data)=>
+    {
+        hihat_buffer = data
+        // ClapBufferNode.buffer=data
+        console.log(data);
+    })
+}
+hihat_rq.send()
+
+var snare_rq = new XMLHttpRequest()
+snare_rq.responseType="arraybuffer"
+snare_rq.open("GET","media/Basic 808 Snare.wav")
+snare_rq.onload = function(event)
+{
+    // console.log(event)
+    ctx.decodeAudioData(snare_rq.response,(data)=>
+    {
+        snare_buffer = data
+        // ClapBufferNode.buffer=data
+        console.log(data);
+    })
+}
+snare_rq.send()
