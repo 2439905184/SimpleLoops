027746cded01e92d411bdfe481452c6ebd4f2202
13023335265 <suchenglong018@126.com>
2023-06-21 13:07:36 +0800

    🐞 fix: 修复了页码不显示问题
    vue js混合使用

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import { createApp } from 'vue'
 import App from "./App.vue"
-// window.ctx = new AudioContext()
 
-// const respone = await fetch("src/assets/Basic 808 Kick.wav")
-// var raw_buffer = await respone.arrayBuffer()
-// const decoded_buffer = await window.ctx.decodeAudioData(raw_buffer)
-// window.kick_buffer = decoded_buffer
-// console.log(kick_buffer);
 
-// window.tick_index = 0
-
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')
+console.log(app);
+// app.mounted(()=>
+// {   
+//     
+// })
